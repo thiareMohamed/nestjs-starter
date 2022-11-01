3d4b9bf1d1cd56999c5eebbdfca7e7f7d9685cb0
thiareMohamed <thiaremohamed.mt@gmail.com>
2022-11-01 15:59:48 +0000

    hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '../entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,6 +13,8 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    createUserDto.password = hashedPassword;
     const user = this.userRepository.create(createUserDto);
     await this.userRepository.save(user);
     return user;
